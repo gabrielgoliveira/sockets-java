fb42f9f7c50a19b2c79bfb5d4363eb700d9202d5
Gabriel Oliveira <gabriel.oliveira@irancho.com.br>
2023-02-01 00:14:27 -0300

    sockets tcp e udp e tratamento de erros

---
+++ TCP/TCPClient.java
@@ -0,0 +1,50 @@
+import java.io.*; 
+import java.net.*; 
+
+class TCPClient { 
+
+  public static void main(String argv[]) throws Exception { 
+    String sentence; 
+    String modifiedSentence; 
+
+    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
+
+    // cria o client socket e tenta se conectar ao servidor
+    
+    try { 
+      Socket clientSocket = new Socket("localhost-teste", 6789); 
+       /*  
+        criar os streams de entrada e saida
+      */ 
+
+      // envia para o servidor
+      DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
+      // recebe do servidor
+      BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+      // Recebe um input do teclado
+      System.out.print("Digite uma mensagem : ");
+      sentence = inFromUser.readLine(); 
+
+      // Envia para o servidor e acrescenta um '\n' no final para indicar que o envio foi finalizado.
+      outToServer.writeBytes(sentence + '\n'); 
+
+      // Recebe uma mensagem do servidor
+      modifiedSentence = inFromServer.readLine(); 
+
+      // Printa a mensagem do servidor
+      System.out.println("FROM SERVER: " + modifiedSentence); 
+
+      // fecha conexão socket
+      clientSocket.close(); 
+
+    } catch(ConnectException e) {
+      // host não está disponivel
+      System.err.println(e.getMessage());
+    } catch (UnknownHostException e) {
+      // não existe o host na rede
+      System.err.println("Host Desconhecido");
+  }
+          
+  } 
+}
---
+++ UDP/UDPClient.java
@@ -0,0 +1,48 @@
+import java.io.*; 
+import java.net.*; 
+  
+class UDPClient { 
+  public static void main(String args[]) throws Exception  { 
+    try {
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
+
+      // cria um datagrama socket que representa um socket udp
+      DatagramSocket clientSocket = new DatagramSocket(); 
+      
+      // cria um objeto ip que vai representar o ip da maquina
+      InetAddress IPAddress = InetAddress.getByName("localhost"); 
+      System.out.println("IP " + IPAddress.getHostAddress());
+
+      byte[] sendData = new byte[1024]; 
+      byte[] receiveData = new byte[1024]; 
+
+      // lê uma string e converte em bytes
+      String sentence = inFromUser.readLine(); 
+      sendData = sentence.getBytes();
+
+      // cria um datagrama
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876); 
+
+      // envia o datagrama utilizando o socket udp
+      clientSocket.send(sendPacket); 
+
+      // lê um datagrama do servidor
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); 
+
+      clientSocket.receive(receivePacket); 
+
+      String modifiedSentence = new String(receivePacket.getData()); 
+
+      System.out.println("FROM SERVER:" + modifiedSentence); 
+      clientSocket.close();
+
+    } catch (SocketException e) {
+      System.out.println("Erro ao criar o socket");
+    } catch (UnknownHostException e) {
+        System.out.println("Host não encontrado");
+    } catch (IOException e) {
+        System.out.println("ConnectionRefused");
+    }
+     
+  } 
+} 
